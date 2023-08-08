5c0f6cf42a034c645730fc1628e7300026416c02
Nitesh_nk <niteshnicknk@gamil.com>
2023-08-09 01:40:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function activeClick (){
+    document.querySelector('.menu__line1').classList.toggle('lineAlt1');
+    document.querySelector('.menu__line2').classList.toggle('lineAlt2');
+    document.querySelector('.menu__line3').classList.toggle('lineAlt3');
+    document.querySelector('.nav-items').classList.toggle('active');
+    document.querySelector('.link').classList.toggle('activeOp');
+    document.querySelector('.link1').classList.toggle('activeOp');
+    document.querySelector('.link2').classList.toggle('activeOp');
+    document.querySelector('.link3').classList.toggle('activeOp');
+        
+}
